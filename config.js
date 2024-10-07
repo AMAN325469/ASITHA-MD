69bcce5a55b0ea9a85d2a77b2bf76461603f9e74
AMAN KUMAR YADAV <113436781+AMAN325469@users.noreply.github.com>
2024-10-07 19:52:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=X7QiGQxQ#wwHA3gJzc-UmpjiE1BJQ7-2vXJhfZxhyL6gAWnMd5nQ",
+MONGODB: process.env.MONGODB || "mongodb+srv://adamlevingaming:adamlevingaming@harshita.0m4yi.mongodb.net/?retryWrites=true&w=majority&appName=HARSHITA",//enter mongo db url
 };
